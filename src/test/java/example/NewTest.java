42f940be857da2451fe00396b9d54826e72ba004
Madhavraob <madhavrao039@gmail.com>
2017-07-23 16:40:06 +0530

    Update NewTest.java

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,7 +1,9 @@
 package example;		
 
-import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;	
+
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;		
 import org.testng.Assert;		
